d648daeb53891f7922ecc3a5fbcdf44faf94db97
zhrasatavohary <ext-zo-hasina.rasatavohary@flowbird.group>
2023-09-04 23:19:02 +0200

    remove target folder

--- target/classes/main/java/org/aquantic/Greeter.java
+++
@@ -1,6 +0,0 @@
-package org.aquantic;
-public class Greeter {
-    public String sayHello() {
-        return "Hello World from Greeter !";
-    }
-}
--- target/classes/main/java/org/aquantic/HelloWorldApp.java
+++
@@ -1,7 +0,0 @@
-package org.aquantic;
-public class HelloWorldApp {
-    public static void main(String[] args) {
-        Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
-    }
-}
